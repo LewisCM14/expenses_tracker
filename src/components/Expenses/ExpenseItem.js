aee49150171a827678ed4762cbf935225b95f01e
LewisCM14 <Lewiscmartin@live.co.uk>
2022-06-08 18:29:07 +0100

    create the NewExpense & ExpenseForm components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-    return (
-      <Card className="expense-item">
-        <ExpenseDate date={ props.date } />
-        <div className="expense-item__description">
-          <h2>{ props.title }</h2>
-          <div className="expense-item__price">£{ props.amount }</div>
-        </div>
-      </Card>
-    );
-}
+
+  // title points at the useState function, sets the initial value
+  // setTitle is the function to update the value later
+  // useState always returns an array with two values,
+  // the first is always the current state value, the second is a function to update it
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+  };
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={ props.date } />
+      <div className="expense-item__description">
+        <h2>{ title }</h2>
+        <div className="expense-item__price">£{ props.amount }</div>
+      </div>
+      <button onClick={ clickHandler }>Change Title</button>
+    </Card>
+  );
+};
 
 export default ExpenseItem;
