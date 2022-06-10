2a9972fea4fafb7fe754c054646a6fcd14b33da8
LewisCM14 <Lewiscmartin@live.co.uk>
2022-06-10 18:51:27 +0100

    handle providing user feedback if no expenses found for date selected

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -21,6 +21,23 @@ const Expenses = (props) => {
         return expense.date.getFullYear().toString() === filteredYear;
     });
 
+
+    /* Sets expensesContent to a user feedback message, once 'if' condition is met
+    handles dynamic display of the expenses */
+    let expensesContent = <p>No expenses found.</p>;
+
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem
+                // Unique identifier is required for dynamic list rendering, preventing console errors.
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ))
+    };
+
     return (
         <div>
             <Card className="expenses">
@@ -28,15 +45,7 @@ const Expenses = (props) => {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem
-                        // Unique identifier is required for dynamic list rendering, preventing console errors.
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />
-                ))}
+                {expensesContent}
             </Card>
         </div>
     );
